b43f769c348c7039c9292d1548393fd9b390d49a
PatelPratik39 <ptl.pratik717@gmail.com>
2024-05-12 12:52:13 -0500

    standalone - false

---
+++ my-app/src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'App-server',
+    templateUrl:'./server.component.html'
+})
+export class ServerComponent{
+
+}
---
+++ my-app2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
